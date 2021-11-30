9d8836612887e5f868902e55162b257a70b37e17
Jiahao XU <Jiahao_XU@outlook.com>
2021-12-01 00:14:58 +1100

    Copy mods from openssh-sftp-client
    Signed-off-by: Jiahao XU <Jiahao_XU@outlook.com>

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,7 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
-    }
-}
+#![forbid(unsafe_code)]
+
+pub mod constants;
+pub mod extensions;
+pub mod file;
+pub mod request;
+pub mod response;
